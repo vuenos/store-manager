80d8ce0486d03f90fd29ddea2de6e0dbbb6ac0bd
woojae.jung <woojae.jung@sellerhub.co.kr>
2022-11-15 10:02:05 +0900

    feat: cardList

--- src/pages/order/OrderList.jsx
+++ src/pages/order/OrderList.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import CardList from '../../components/CardList'
 
 const OrderList = () => {
   return (
     <div>
       <h1>OrderList</h1>
+      <CardList />
     </div>
   )
 }
