bd393d0bad5e859aa97e759c14228b27c7a891cd
woojae.jung <woojae.jung@sellerhub.co.kr>
2022-12-23 14:06:15 +0900

    feat: 중간커밋

--- src/pages/order/OrderList.jsx
+++ src/pages/order/OrderList.jsx
@@ -1,76 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CardList from 'components/CardList'
 import Tabs2 from 'components/Tabs2';
 import Search from 'components/Search/Search'
+import { useEffect } from 'react';
+
 
 const OrderList = () => {
 
-  const data =[
-      {
-          index: 1,
-          icon:"order-pay",
-          title:"결제완료",
-          count:12,
-          subject:"payed",         
-      },
-      {
-          index: 2,
-          icon:"order-ready",
-          title:"배송준비중",
-          count:397,
-          subject:"delivery_waiting"
-      },
-      {
-          index: 3,
-          icon:"order-truck",
-          title:"배송중",
-          count:2,
-          subject:"delivering"
-      },
-      {
-          index: 4,
-          icon:"order-delivery",
-          title:"배송완료",
-          count:6,
-          subject:"delivered"          
-      },        
-      {
-          index: 5,
-          icon:"order-confirm",
-          title:"구매확정",
-          count:12,
-          subject:"purchase_decided"
-      }
-  ];
+    const data =[
+        {
+            index: 1,
+            icon:"order-pay",
+            title:"결제완료",
+            count:12,
+            subject:"payed",         
+        },
+        {
+            index: 2,
+            icon:"order-ready",
+            title:"배송준비중",
+            count:397,
+            subject:"delivery_waiting"
+        },
+        {
+            index: 3,
+            icon:"order-truck",
+            title:"배송중",
+            count:2,
+            subject:"delivering"
+        },
+        {
+            index: 4,
+            icon:"order-delivery",
+            title:"배송완료",
+            count:6,
+            subject:"delivered"          
+        },        
+        {
+            index: 5,
+            icon:"order-confirm",
+            title:"구매확정",
+            count:12,
+            subject:"purchase_decided"
+        }
+    ];
 
-  const search = [
-    {
-        index: 1,
-        title: '기간검색'
-    },
-    {
-        index: 2,
-        title: '연동몰/계정'
-    }
-  ]
+    const search = [
+        {
+            index: 1,
+            title: '기간검색'
+        },
+        {
+            index: 2,
+            title: '연동몰/계정'
+        }
+    ];
 
 
-  return (
-  
-    <div>
-      <h1>OrderList</h1>
+    return (
 
-      <Tabs2 
-        data={data}
-      />
+        <div>      
 
-      <Search 
-        search={search}
-      />
+            <Tabs2 
+                data={data}
+            />
 
-      <CardList />
+            <Search 
+                search={search}
+            />
 
-    </div>
+            <CardList />
+
+        </div>
 
     )
 }
