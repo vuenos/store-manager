25b078aa8f1bdd39dc5d23d774e30b375946c9e4
jintae kim <jintae.kim@sellerhub.co.kr>
2023-01-13 16:30:25 +0900

    Table components antd apply

--- src/api/getQnaActions.js
+++ src/api/getQnaActions.js
@@ -1,18 +1,24 @@
 import apiClient from "../services/api";
 import axios from "axios";
 
-const token = localStorage.getItem("access_token");
-const config = {
-  headers: {
-    Authorization: "Bearer " + token
-  }
-}
+// const token = localStorage.getItem("access_token");
+// const config = {
+//   headers: {
+//     Authorization: "Bearer " + token
+//   }
+// }
 
 export const getQnaList = async () => {
   // const { data } = await apiClient.get(`/sellers/products?page=${pageNum}`, config);
   // console.log('QNA_DATA:::', data.data);
   // return data.data;
 
-  const res = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
-  return res.data;
+  const token = localStorage.getItem("access_token");
+  const { data } = await apiClient.get(`/sellers/inquiries`, {
+    headers: {
+      Authorization: "Bearer " + token
+    }
+  });
+  console.log('DATA', data)
+  return data.data;
 }
